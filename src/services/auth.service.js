22a8386491d584c19370378c7d9edc0effe47e9e
Manuel Lorenzo <manuelalejandrolorenzozambrano@gmail.com>
2025-05-28 20:16:47 -0400

    cambiar JWT_EXPIRES_IN

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
 const userRepo = AppDataSource.getRepository(User);
-const EXPIRES_IN = process.env.EXPIRES_IN || '1h';
+const EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h';
 export const registerUser = async ({ email, password }) => {
 
   const existingUser = await userRepo.findOneBy({ email });
